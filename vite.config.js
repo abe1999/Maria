750f284dce5ed92f6f00ddc8cd716090b5344b49
abe1999 <abepro10@hotmail.com>
2025-08-14 23:19:37 -0300

    adicionado repositorio automatico

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // Configuração 'base' para o GitHub Pages
+  base: "/Maria/",
+
+  plugins: [],
+});

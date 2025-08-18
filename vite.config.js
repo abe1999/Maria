cd390e628a45b500a4331dcdca23476328a9b8b0
abe1999 <abepro10@hotmail.com>
2025-08-18 08:50:37 -0400

    teste.index

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/Maria/", // Seu repositório
+  base: "/", // Seu repositório
   build: {
     rollupOptions: {
       input: {
